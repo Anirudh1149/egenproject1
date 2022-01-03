48a2506581dc6469d83ed4ea15fb61fa2104413d
ANIRUDH EDUPUGANTI <anirudh.edupuganti@gmail.com>
2022-01-03 10:15:17 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import webapp2
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("helloo-Egen")
+app = webapp2.WSGIApplication([('/',MainHandler)], debug = True)
